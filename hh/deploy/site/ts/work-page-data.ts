d5f08e2cff3a9c996ed26a8635fdff70a6af3a80
Lee Krasnow <lee.krasnow@gmail.com>
2025-11-17 23:28:45 -0500

    fiexe?

--- hh/deploy/site/ts/work-page-data.ts
+++ hh/deploy/site/ts/work-page-data.ts
@@ -299,7 +299,7 @@ export class WorkPageData extends PageData {
                 success: true,
                 _showMessage: 'Meta updated successfully',
                 _autoFade: !hasDebugData, // Disable auto-fade if debug data is present
-                debug: result.debug
+                // Don't return debug here - we already showed it via handleRPCResponseWithDebug
               };
             } else {
               // Server returned error - include debug data if present
