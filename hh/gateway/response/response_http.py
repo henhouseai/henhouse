b2963737a3b97507704aded3ba4248059833fe34
Lee Krasnow <lee.krasnow@gmail.com>
2025-12-21 15:23:36 -0500

    more tweaks

--- hh/gateway/response/response_http.py
+++ hh/gateway/response/response_http.py
@@ -121,7 +121,7 @@ class ResponseHTTP(Response):
         html = f"""<!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes, viewport-fit=cover">
     <title>{title}</title>
 {css_section}
 {seed_script}
